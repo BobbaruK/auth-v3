ada0fa5a1f935ee0aba4eaa6f8443e6105e3673c
Bobbaru-K <bobbaru311@gmail.com>
2025-09-07 02:22:52 +0300

    chore(boundaries): setup eslint boundaries

--- next-app/eslint.config.mjs
+++ next-app/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import boundaries from "eslint-plugin-boundaries";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,6 +20,74 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    plugins: {
+      boundaries,
+    },
+    settings: {
+      "boundaries/include": ["src/**/*"],
+      "boundaries/elements": [
+        {
+          mode: "full",
+          type: "core",
+          capture: ["name"],
+          pattern: ["src/core/**/*"],
+        },
+        {
+          mode: "full",
+          type: "shared",
+          pattern: ["src/components/**/*"],
+        },
+        {
+          mode: "full",
+          type: "feature",
+          capture: ["name"],
+          pattern: ["src/features/*/**/*"],
+        },
+        {
+          mode: "full",
+          type: "app",
+          capture: ["_", "fileName"],
+          pattern: ["src/app/**/*"],
+        },
+        {
+          mode: "full",
+          type: "neverImport",
+          pattern: ["src/*"],
+        },
+      ],
+    },
+    rules: {
+      "boundaries/no-unknown": [2],
+      "boundaries/no-unknown-files": [2],
+      "boundaries/element-types": [
+        2,
+        {
+          default: "disallow",
+          rules: [
+            {
+              from: "core",
+              allow: ["core"],
+            },
+            {
+              from: "shared",
+              allow: ["core", "shared"],
+            },
+            {
+              from: "feature",
+              allow: ["core", "shared"],
+            },
+            {
+              from: ["app", "neverImport"],
+              allow: ["core", "shared", "feature"],
+            },
+            {
+              from: ["app"],
+              allow: [["app", { fileName: "*.css" }]],
+            },
+          ],
+        },
+      ],
+    },
   },
 ];
 
