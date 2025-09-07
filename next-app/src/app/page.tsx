9bd4ea1b073284b7fdff380d95412ac9650c6564
Bobbaru-K <bobbaru311@gmail.com>
2025-09-07 22:22:03 +0300

    chore(home): add title to home page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -3,11 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="container py-4 flex items-center gap-4">
-      <Button variant={"default"} effect={"gradientSlideShow"}>
-        Hello World
-      </Button>
-      <ModeToggle />
+    <div className="container py-4 flex-col flex items-center gap-4">
+      <h1>Boilerplate for better auth</h1>
+      <div className="flex items-center gap-4">
+        <Button variant={"default"} effect={"gradientSlideShow"}>
+          Hello World
+        </Button>
+        <ModeToggle />
+      </div>
     </div>
   );
 }
