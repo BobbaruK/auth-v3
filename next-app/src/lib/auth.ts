3f63e6cc8c24c0148c78b690cc4da6b012da3d06
Bobbaru-K <bobbaru311@gmail.com>
2025-09-09 01:58:38 +0300

    feat(auth): set better auth for credentials

--- next-app/src/lib/auth.ts
+++ next-app/src/lib/auth.ts
@@ -1,9 +1,26 @@
+import { MIN_PASSWORD } from "@/constants/misc";
+import prisma from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  user: {
+    modelName: "auth_user",
+  },
+  account: {
+    modelName: "auth_account",
+  },
+  session: {
+    modelName: "auth_session",
+  },
+  verification: {
+    modelName: "auth_verification",
+  },
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: MIN_PASSWORD,
+  },
 });
