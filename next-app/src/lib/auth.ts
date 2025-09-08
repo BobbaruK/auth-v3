69e5fb95c5fd305a309f7144b20833c2cd555a38
Bobbaru-K <bobbaru311@gmail.com>
2025-09-09 02:37:01 +0300

    fix(auth): remove duplicated code

--- next-app/src/lib/auth.ts
+++ next-app/src/lib/auth.ts
@@ -2,7 +2,6 @@ import { MIN_PASSWORD } from "@/constants/misc";
 import prisma from "@/lib/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
