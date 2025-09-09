5f202faf7db44b3945e13bea1d8865041208bc17
Bobbaru-K <bobbaru311@gmail.com>
2025-09-09 23:49:28 +0300

    fix(auth): update auth-client logic

--- next-app/src/lib/auth-client.ts
+++ next-app/src/lib/auth-client.ts
@@ -5,4 +5,4 @@ const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-export const { signUp, signOut } = authClient;
+export const { signUp, signOut, signIn, useSession } = authClient;
