82a5ae650e7f5f5d0ad35a86e6130b6404722e21
Bobbaru-K <bobbaru311@gmail.com>
2025-09-10 15:13:33 +0300

    refactor(auth): update sign out client button

--- next-app/src/core/auth/components/sign-out-button.tsx
+++ next-app/src/core/auth/components/sign-out-button.tsx
@@ -3,27 +3,38 @@
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useTransition } from "react";
 import { toast } from "sonner";
 
 export const SignOutButton = () => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-  const handleClick = async () => {
-    await signOut({
-      fetchOptions: {
-        onError: (ctx) => {
-          toast.error(ctx.error.message);
+  const handleClick = () => {
+    startTransition(async () => {
+      await signOut({
+        fetchOptions: {
+          onError: (ctx) => {
+            toast.error(ctx.error.message);
+          },
+          onRequest: () => {
+            toast.warning("Logging out...");
+          },
+          onSuccess: () => {
+            toast.success("You have successfully logout");
+            router.push("/login");
+          },
         },
-        onSuccess: () => {
-          router.push("/login");
-        },
-      },
+      });
     });
   };
 
   return (
-    <Button onClick={handleClick} size={"sm"} variant={"destructive"}>
+    <Button
+      onClick={handleClick}
+      size={"sm"}
+      variant={"destructive"}
+      disabled={isPending}>
       Sign Out
     </Button>
   );
