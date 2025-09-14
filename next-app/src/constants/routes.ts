7920b78adefd6be6b4e379af0dc21cdd22a1cdc4
Bobbaru-K <bobbaru311@gmail.com>
2025-09-15 00:58:15 +0300

    feat(auth): update public routes

--- next-app/src/constants/routes.ts
+++ next-app/src/constants/routes.ts
@@ -23,7 +23,7 @@ export const API_AUTH_PREFIX = "/api/auth";
  * These routes do not require authentication
  * @type {string[]}
  */
-export const PUBLIC_ROUTES = ["/", "/admin"];
+export const PUBLIC_ROUTES = ["/"];
 
 /**
  * An array of routes that are forbidden to all users
