c2e899781a8943da76d7917c0582df98c20f12e2
Bobbaru-K <bobbaru311@gmail.com>
2025-09-19 02:09:57 +0300

    feat(app): update home page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,13 +1,14 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container py-4 flex-col flex items-center gap-4">
       <h1>Boilerplate for better auth</h1>
       <div className="flex items-center gap-4">
-        <Button variant={"default"} effect={"gradientSlideShow"}>
-          Hello World
+        <Button variant={"default"} effect={"gradientSlideShow"} asChild>
+          <Link href={"/login"}>Hello World</Link>
         </Button>
         <ModeToggle />
       </div>
