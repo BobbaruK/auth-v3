7045a5bf0976feaf2904a2a9581f895b53c7ba75
Bobbaru-K <bobbaru311@gmail.com>
2025-09-20 04:16:55 +0300

    refactor(ui): update input otp component

--- next-app/src/core/auth/components/otp-verification-form.tsx
+++ next-app/src/core/auth/components/otp-verification-form.tsx
@@ -16,18 +16,21 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { MESSAGES } from "@/constants/messages";
 import { DEFAULT_LOGIN_REDIRECT } from "@/constants/routes";
-import { signOut, twoFactor } from "@/lib/auth-client";
+import { twoFactor } from "@/lib/auth-client";
+import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useTransition } from "react";
+import React, { useEffect, useTransition } from "react";
 import { useForm } from "react-hook-form";
+import { IoIosCopy } from "react-icons/io";
 import QRCode from "react-qr-code";
 import { toast } from "sonner";
+import { useCopyToClipboard } from "usehooks-ts";
 import z from "zod";
 import { clearTwoFactorCookie } from "../actions/clear-two-factor-cookie";
 import { OTP } from "../schemas/otp";
-import { MESSAGES } from "@/constants/messages";
 
 interface Props {
   otpLink: string | null;
@@ -37,6 +40,9 @@ interface Props {
 export const OTPVerificationForm = ({ otpLink, isFirstTime }: Props) => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [copiedText, copy] = useCopyToClipboard();
+
   const form = useForm<z.infer<typeof OTP>>({
     resolver: zodResolver(OTP),
     defaultValues: {
@@ -44,6 +50,9 @@ export const OTPVerificationForm = ({ otpLink, isFirstTime }: Props) => {
       remember: false,
     },
   });
+
+  const url = otpLink ? new URL(otpLink) : "";
+  const secret = url && url.searchParams.get("secret");
 
   const onSubmit = (values: z.infer<typeof OTP>) => {
     startTransition(async () => {
@@ -62,16 +71,9 @@ export const OTPVerificationForm = ({ otpLink, isFirstTime }: Props) => {
           return;
         }
 
-        // TODO: test this
-        if (isFirstTime) {
-          // await signOut();
-          toast.success(MESSAGES.QR_VALIDATED);
-          router.push(DEFAULT_LOGIN_REDIRECT);
-          router.refresh();
-          return;
-        }
-
-        toast.success(MESSAGES.LOGIN_SUCCESS);
+        toast.success(
+          isFirstTime ? MESSAGES.QR_VALIDATED : MESSAGES.LOGIN_SUCCESS
+        );
         router.push(DEFAULT_LOGIN_REDIRECT);
         router.refresh();
       } catch (error) {
@@ -80,9 +82,55 @@ export const OTPVerificationForm = ({ otpLink, isFirstTime }: Props) => {
     });
   };
 
+  const handleCopy = (text: string | null) => () => {
+    if (!text) {
+      toast.error("Nothing to copy");
+      return;
+    }
+
+    copy(text)
+      .then(() => {
+        toast.success("Copied", {
+          description: <div className="line-clamp-1">{copiedText}</div>,
+        });
+      })
+      .catch((error) => {
+        toast.error("Failed to copy!");
+      });
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
   return (
     <div className="flex flex-col gap-4">
-      {otpLink && <QRCode value={otpLink} className="w-full" />}
+      {otpLink && (
+        <>
+          <QRCode value={otpLink} className="w-full" />
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="bg-card text-muted-foreground relative z-10 px-2">
+              OR
+            </span>
+          </div>
+          <p>
+            If you can&apos;t use the QR code, enter this secret key manually in
+            your authenticator app.
+          </p>
+          <div className="flex gap-2 items-center">
+            <p className={cn("truncate w-full")}>{secret}</p>
+            <CustomButton
+              buttonLabel={`Copy secret`}
+              variant={"secondary"}
+              size={"icon"}
+              icon={IoIosCopy}
+              iconPlacement="left"
+              className="size-10 min-w-10"
+              onClick={handleCopy(secret)}
+            />
+          </div>
+        </>
+      )}
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <FormField
@@ -92,7 +140,7 @@ export const OTPVerificationForm = ({ otpLink, isFirstTime }: Props) => {
               <FormItem>
                 <FormLabel>One Time Password</FormLabel>
                 <FormControl>
-                  <InputOTP maxLength={6} {...field}>
+                  <InputOTP maxLength={6} {...field} ref={inputRef}>
                     <InputOTPGroup>
                       <InputOTPSlot index={0} />
                       <InputOTPSlot index={1} />
