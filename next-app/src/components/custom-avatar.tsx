12681587a384fc22430a031c74a104bdb2f24127
Bobbaru-K <bobbaru311@gmail.com>
2025-09-20 23:24:33 +0300

    feat(ui): create custom avatar component

---
+++ next-app/src/components/custom-avatar.tsx
@@ -0,0 +1,76 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { FaUser } from "react-icons/fa";
+
+interface Props extends React.HTMLAttributes<HTMLSpanElement> {
+  image?: string | null;
+  fit?: "cover" | "contain";
+  icon?: React.ReactNode;
+  position?:
+    | "top"
+    | "left-top"
+    | "right-top"
+    | "right"
+    | "right-bottom"
+    | "bottom"
+    | "left-bottom"
+    | "left"
+    | "left-top";
+}
+
+export const CustomAvatar = ({
+  image,
+  fit,
+  icon,
+  position,
+  ...restProps
+}: Props) => {
+  const objectPosition = () => {
+    switch (position) {
+      case "top":
+        return "object-top";
+
+      case "right-top":
+        return "object-right-top";
+
+      case "left-top":
+        return "object-left-top";
+
+      case "right":
+        return "object-right";
+
+      case "right-bottom":
+        return "object-right-bottom";
+
+      case "bottom":
+        return "object-bottom";
+
+      case "left-bottom":
+        return "object-left-bottom";
+
+      case "left":
+        return "object-left";
+
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <Avatar
+      {...restProps}
+      className={cn("border-primary max-w-full border", restProps.className)}
+    >
+      <AvatarImage
+        src={image || undefined}
+        className={cn(
+          objectPosition(),
+          fit === "cover" || !fit ? "object-cover" : "object-contain",
+        )}
+      />
+      <AvatarFallback className="rounded-none">
+        {icon || <FaUser className="h-[55%] w-[55%]" />}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
