348beebade14fdcc4ed5ff7757c974a35ce8ddfc
Bobbaru-K <bobbaru311@gmail.com>
2025-09-22 05:39:28 +0300

    chore(schema): create username schema

---
+++ next-app/src/schemas/username.ts
@@ -0,0 +1,15 @@
+import { MAX_USERNAME, MIN_USERNAME } from "@/constants/misc";
+import z from "zod";
+
+export const USERNAME_SCHEMA = z
+  .string()
+  .min(MIN_USERNAME, {
+    message: `Username must be ${MIN_USERNAME} or more characters long`,
+  })
+  .max(MAX_USERNAME, {
+    message: `Username must be ${MAX_USERNAME} or fewer characters long`,
+  })
+  .regex(
+    /^[a-zA-Z0-9._]+$/,
+    "Username can only contain alphanumeric characters, underscores, and dots",
+  );
