1a0fcd488f6db5a4cf7fc014d06abc6bf83b457d
Bobbaru-K <bobbaru311@gmail.com>
2025-09-23 04:22:22 +0300

    chore(ui): update user info component

--- next-app/src/components/user-info.tsx
+++ next-app/src/components/user-info.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { UserSession } from "@/types/session";
+import { Badge } from "./ui/badge";
 
 interface Props {
   user?: UserSession | null;
@@ -22,12 +23,30 @@ export const UserInfo = ({ user }: Props) => {
           </p>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">First Name</p>
+          <p className="max-w-[180px] truncate rounded-md bg-slate-100 p-1 font-mono text-xs text-black">
+            {user?.firstName}
+          </p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Last Name</p>
+          <p className="max-w-[180px] truncate rounded-md bg-slate-100 p-1 font-mono text-xs text-black">
+            {user?.lastName}
+          </p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Username</p>
+          <p className="max-w-[180px] truncate rounded-md bg-slate-100 p-1 font-mono text-xs text-black">
+            {user?.displayUsername}
+          </p>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Email</p>
           <p className="max-w-[180px] truncate rounded-md bg-slate-100 p-1 font-mono text-xs text-black">
             {user?.email}
           </p>
         </div>
-        {/* <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Role</p>
           <p className="max-w-[180px] truncate rounded-md bg-slate-100 p-1 font-mono text-xs text-black">
             {user?.role}
@@ -35,10 +54,10 @@ export const UserInfo = ({ user }: Props) => {
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">2FA</p>
-          <Badge variant={user?.isTwoFactorEnabled ? "default" : "destructive"}>
-            {user?.isTwoFactorEnabled ? "ON" : "OFF"}
+          <Badge variant={user?.twoFactorEnabled ? "default" : "destructive"}>
+            {user?.twoFactorEnabled ? "ON" : "OFF"}
           </Badge>
-        </div> */}
+        </div>
       </CardContent>
     </Card>
   );
