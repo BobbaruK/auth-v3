2c4a4af852653875f9a21b835b0cc0c09d01c280
Bobbaru-K <bobbaru311@gmail.com>
2025-09-28 01:22:33 +0300

    feat(profile): update profile page

--- next-app/src/app/(site)/(protected)/profile/[userId]/page.tsx
+++ next-app/src/app/(site)/(protected)/profile/[userId]/page.tsx
@@ -1,6 +1,11 @@
+import { CustomAlert } from "@/components/custom-alert";
 import { PageStructure } from "@/components/page-structure";
+import { MESSAGES } from "@/constants/messages";
 import { getUser } from "@/core/user/data/get-user";
 import { ProfileHeader } from "@/features/settings/components/profile-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { MdOutlineAccountBox } from "react-icons/md";
 
 interface Props {
   params: Promise<{ userId: string }>;
@@ -8,12 +13,72 @@ interface Props {
 
 const ProfilePage = async ({ params }: Props) => {
   const { userId } = await params;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   const user = await getUser(userId);
 
+  const sameUser = () => (session?.user.id === userId ? true : false);
+  const isSameUser = sameUser();
+
+  if (!user)
+    return (
+      <PageStructure>
+        <CustomAlert
+          title={"Error!"}
+          description={MESSAGES.USER_NOT_EXIST}
+          variant="destructive"
+        />
+      </PageStructure>
+    );
+
   return (
     <PageStructure>
-      <ProfileHeader user={user} />
+      {isSameUser && !user.isAccountVisible && (
+        <CustomAlert
+          title={"Attention!"}
+          description={"Your account visibility is set tot private."}
+          variant="warning"
+          icon={<MdOutlineAccountBox />}
+        />
+      )}
+      <ProfileHeader
+        data={{
+          firstName: isSameUser
+            ? user.firstName
+            : user.isAccountVisible
+              ? user.firstName
+              : undefined,
+          lastName: isSameUser
+            ? user.lastName
+            : user.isAccountVisible
+              ? user.lastName
+              : undefined,
+          username: user.displayUsername || user.firstName,
+          role: isSameUser
+            ? user.role
+            : user.isAccountVisible
+              ? user.role
+              : undefined,
+          image: user.image,
+          meta: isSameUser
+            ? {
+                email: user.email,
+                joined: user.createdAt,
+              }
+            : user.isAccountVisible
+              ? {
+                  email: user.email,
+                  joined: user.createdAt,
+                }
+              : undefined,
+        }}
+      />
+
+      {user.isAccountVisible && (
+        <p>{user.bio || "This user does not have a bio yet."}</p>
+      )}
     </PageStructure>
   );
 };
