d58d9e59872cb805dcaf03cd999dd973c3878938
Bobbaru-K <bobbaru311@gmail.com>
2025-09-28 17:04:46 +0300

    feat(settings): create set password form

---
+++ next-app/src/features/settings/components/profile-content/security/set-password-form.tsx
@@ -0,0 +1,122 @@
+import { CustomButton } from "@/components/custom-button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { PasswordInput } from "@/components/ui/password-input";
+import { MESSAGES } from "@/constants/messages";
+import { setPassword } from "@/core/auth/actions/set-password";
+import { NewPasswordSchema } from "@/core/auth/schemas/new-password";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLFormElement> {
+  closeDialog: () => void;
+}
+
+export const SetPasswordForm = ({ closeDialog, ...restProps }: Props) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: process.env.NEXT_PUBLIC_DEFAULT_REGISTER_PASSWORD || "",
+      confirmPassword: process.env.NEXT_PUBLIC_DEFAULT_REGISTER_PASSWORD || "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    startTransition(async () => {
+      await setPassword(values)
+        .then((data) => {
+          if (data.error) {
+            toast.error(data.error);
+          }
+          if (data.success) {
+            router.refresh();
+
+            setTimeout(() => {
+              toast.success(data.success);
+            }, 200);
+          }
+        })
+        .catch(() => {
+          toast.error(MESSAGES.SOMETHING_WRONG);
+          closeDialog();
+        })
+        .finally(() => {
+          closeDialog();
+        });
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(restProps.className, "space-y-6")}
+      >
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    id="password"
+                    placeholder="******"
+                    autoComplete="new-password"
+                    disabled={isPending}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="confirmPassword">
+                  Confirm Password
+                </FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    id="confirmPassword"
+                    placeholder="******"
+                    autoComplete="new-password"
+                    disabled={isPending}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <CustomButton
+          buttonLabel={`Set password`}
+          type="submit"
+          className="w-full"
+          disabled={isPending}
+          skeletonClassName="w-full"
+        />
+      </form>
+    </Form>
+  );
+};
