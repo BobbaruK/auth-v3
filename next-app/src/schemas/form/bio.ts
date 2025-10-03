c7e93f4bde00675c8ec17b2c32e9f256949f9282
Bobbaru-K <bobbaru311@gmail.com>
2025-10-03 22:15:03 +0300

    chore(schema): update bio schema

--- next-app/src/schemas/form/bio.ts
+++ next-app/src/schemas/form/bio.ts
@@ -1,11 +1,8 @@
-import { MIN_BIO, MAX_BIO } from "@/constants/misc";
+import { MAX_BIO } from "@/constants/misc";
 import z from "zod";
 
 export const BIO = z
   .string()
-  .min(MIN_BIO, {
-    message: `Bio must be at least ${MIN_BIO} characters.`,
-  })
   .max(MAX_BIO, {
     message: `Bio must not be longer than ${MAX_BIO} characters.`,
   })
