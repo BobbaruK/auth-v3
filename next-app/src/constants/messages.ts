9179cbd03f0cb19ff689a6480fcfbce193bfa4cd
Bobbaru-K <bobbaru311@gmail.com>
2025-10-04 02:17:13 +0300

    chore(constants): update messages

--- next-app/src/constants/messages.ts
+++ next-app/src/constants/messages.ts
@@ -1,8 +1,9 @@
 const MESSAGES_FN = () => {
   return {
-    EMAIL_CHANGED: "Your email has been changed. Please verify your email.",
-    ENTER_OTP: "Enter the OTP.",
-    INVALID_FIELDS: "Some fields are invalid.",
+    BACKUPCODE_SUCCESS: "Your backup code has been successfully validated.",
+    EMAIL_CHANGED: "Your email has been changed. Please verify it.",
+    ENTER_OTP: "Please enter the OTP.",
+    INVALID_FIELDS: "Some fields are invalid. Check and try again.",
     LOGIN_SUCCESS: "Login successful. Welcome back!",
     LOGOUT_SUCCESS: "You have successfully logged out.",
     PASSWORD_NEW:
@@ -10,20 +11,20 @@ const MESSAGES_FN = () => {
     PASSWORD_RESET:
       "Success! A confirmation email has been sent to reset your password.",
     PASSWORD_SET:
-      "Your password has been set. You can now login with credentials.",
+      "Your password has been set. You can now log in with your credentials.",
     PASSWORDS_NOT_MATCH: "Passwords do not match.",
-    PROFILE_UPDATED: "Profile updated!",
+    PROFILE_UPDATED: "Your profile has been updated.",
     QR_SCAN: "Scan the QR code below and enter the OTP.",
-    QR_VALIDATED: "QR code validated.",
+    QR_VALIDATED: "QR code successfully validated.",
     REGISTRATION_SUCCESS: "Registration complete. Please verify your email.",
-    SESSION_EXPIRED: "This session is expired. Please login.",
+    SESSION_EXPIRED: "Your session has expired. Please log in again.",
     SOMETHING_WRONG: "Something went wrong. Please try again later.",
-    TOKEN_INVALID: "The token is invalid.",
-    TOKEN_MISSING: "Token is missing.",
+    TOKEN_INVALID: "The token provided is invalid.",
+    TOKEN_MISSING: "No token provided.",
     TWO_FACTOR_DISABLED:
-      "You have successfully disabled 2 factor authentication.",
-    USER_NOT_EXIST: "User does not exist.",
-    USERNAME_NOT_AVAILABLE: "Username is not available.",
+      "Two-factor authentication has been successfully disabled.",
+    USER_NOT_EXIST: "This user does not exist.",
+    USERNAME_NOT_AVAILABLE: "This username is not available.",
   };
 };
 
