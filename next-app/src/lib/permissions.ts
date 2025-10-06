d133693c50464a0288fb541329a202957b3f5ca0
Bobbaru-K <bobbaru311@gmail.com>
2025-10-06 17:29:50 +0300

    chore(auth): update permissions

--- next-app/src/lib/permissions.ts
+++ next-app/src/lib/permissions.ts
@@ -1,40 +1,28 @@
 import { UserRole } from "@/generated/prisma";
 import { createAccessControl } from "better-auth/plugins/access";
-import { adminAc, defaultStatements } from "better-auth/plugins/admin/access";
+import {
+  userAc,
+  adminAc,
+  defaultStatements,
+} from "better-auth/plugins/admin/access";
 
 const statement = {
   ...defaultStatements,
-  users: [
-    "create",
-    "read",
-    "update",
-    "delete",
-    "update:own",
-    "delete:own",
-    "delete:admin",
-  ],
 } as const;
 
 export const ac = createAccessControl(statement);
 
 export const roles = {
   [UserRole.USER]: ac.newRole({
-    users: ["create", "read", "update:own", "delete:own"],
+    ...userAc.statements,
+    user: [...userAc.statements.user],
   }),
   [UserRole.ADMIN]: ac.newRole({
     ...adminAc.statements,
-    users: ["create", "read", "update", "delete", "update:own", "delete:own"],
+    user: ["list", "create", "delete", "ban"],
   }),
   [UserRole.OWNER]: ac.newRole({
     ...adminAc.statements,
-    users: [
-      "create",
-      "read",
-      "update",
-      "delete",
-      "update:own",
-      "delete:own",
-      "delete:admin",
-    ],
+    user: [...adminAc.statements.user],
   }),
 };
