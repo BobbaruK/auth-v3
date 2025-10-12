4fbb171b7d73a91410658dae2c2593f2fefa031c
Bobbaru-K <bobbaru311@gmail.com>
2025-10-12 22:27:55 +0300

    chore(app): update home page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,23 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@/components/user-button";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import Link from "next/link";
+import { Header } from "@/components/layout/header";
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
   return (
-    <div className="container flex flex-col items-center gap-4 py-4">
-      <h1>Boilerplate for better auth</h1>
-      <div className="flex items-center gap-4">
-        <Button variant={"default"} effect={"gradientSlideShow"} asChild>
-          <Link href={"/login"}>Hello World</Link>
-        </Button>
-        <UserButton session={session} />
+    <>
+      <Header />
+      <div className="container flex flex-col items-center gap-4 py-4">
+        <h1>Boilerplate for better auth</h1>
       </div>
-    </div>
+    </>
   );
 }
