498a7f0a4c3e34d02011a98c8a7f714b2ca77772
Bobbaru-K <bobbaru311@gmail.com>
2025-10-12 23:00:57 +0300

    chore(app): update admin testing page

--- next-app/src/app/(site)/(protected)/admin/page.tsx
+++ next-app/src/app/(site)/(protected)/admin/page.tsx
@@ -1,9 +1,67 @@
+"use client";
+
+import { CustomAlert } from "@/components/custom-alert";
+import { CustomButton } from "@/components/custom-button";
 import { PageStructure } from "@/components/page-structure";
+import { RoleGate } from "@/components/role-gate";
+import { Card, CardContent } from "@/components/ui/card";
+import { admin } from "@/core/auth/actions/admin";
+import { UserRole } from "@/generated/prisma";
+import { useSession } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 const AdminPage = () => {
+  const { data: session } = useSession();
+  const role = session?.user.role as UserRole;
+
+  const onApiRouteClick = () => {
+    fetch("/api/admin").then((res) => {
+      if (res.ok) {
+        toast.success("Allow API Route!");
+      } else {
+        toast.error("Forbidden API Route!");
+      }
+    });
+  };
+
+  const onServerActionClick = () => {
+    admin(role).then((data) => {
+      if (data.success) toast.success(data.success);
+      if (data.error) toast.error(data.error);
+    });
+  };
+
   return (
     <PageStructure>
       <h1 className="text-3xl font-bold">Admin</h1>
+
+      <Card>
+        <CardContent className="space-y-4">
+          <RoleGate allowedRole={[UserRole.ADMIN, UserRole.OWNER]}>
+            <CustomAlert
+              variant={"info"}
+              title="Allowed"
+              description="You are allowed to see this content!"
+            />
+          </RoleGate>
+          <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+            <p className="text-sm font-medium">Admin-only API Route</p>
+            <CustomButton
+              buttonLabel="Click to test"
+              variant={"link"}
+              onClick={onApiRouteClick}
+            />
+          </div>
+          <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+            <p className="text-sm font-medium">Admin-only Server Action</p>
+            <CustomButton
+              buttonLabel="Click to test"
+              variant={"link"}
+              onClick={onServerActionClick}
+            />
+          </div>
+        </CardContent>
+      </Card>
     </PageStructure>
   );
 };
