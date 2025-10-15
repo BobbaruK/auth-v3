350e861748962c6138cd817505b78db0e5293774
Bobbaru-K <bobbaru311@gmail.com>
2025-10-15 23:49:33 +0300

    chore(app): encapsulate catch error for server actions

---
+++ next-app/src/lib/utils/catch-error-action.ts
@@ -0,0 +1,24 @@
+import { ErrorCode } from "@/types/errors";
+import { APIError } from "better-auth/api";
+
+export const catchError = (error: unknown) => {
+  console.error("Something went wrong: ", JSON.stringify(error));
+
+  if (error instanceof APIError) {
+    const errCode = error.body?.code as ErrorCode;
+
+    switch (errCode) {
+      // case "USER_ALREADY_EXISTS":
+      //   return {
+      //     error: "Oops! Something went wrong. Please try again.",
+      //   };
+
+      default:
+        return {
+          error: error.message,
+        };
+    }
+  }
+
+  throw error;
+};
