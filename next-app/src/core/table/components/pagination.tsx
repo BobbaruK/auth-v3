337a3b0ad10992ffd9f6c0b2c8f202e914471c9f
Bobbaru-K <bobbaru311@gmail.com>
2025-10-18 03:11:23 +0300

    feat(table): update pagination

--- next-app/src/core/table/components/pagination.tsx
+++ next-app/src/core/table/components/pagination.tsx
@@ -1,3 +1,11 @@
+import { CustomButton } from "@/components/custom-button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Select,
   SelectContent,
@@ -5,24 +13,98 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+import { PAGINATION_ARR } from "@/constants/table";
+import { banUser, unbanUser } from "@/core/auth/actions/ban-user";
+import { useSearchParams } from "@/hooks/use-search-params";
+import { chunkArray } from "@/lib/utils/chunk-array";
 import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
 import {
   MdKeyboardDoubleArrowLeft,
   MdKeyboardDoubleArrowRight,
 } from "react-icons/md";
-
-import { CustomButton } from "@/components/custom-button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { PAGINATION_ARR } from "@/constants/table";
-import { useSearchParams } from "@/hooks/use-search-params";
+import { toast } from "sonner";
+import { useCopyToClipboard } from "usehooks-ts";
 import { useTableContext } from "../providers/table-provider";
 
 export function DataTablePagination() {
-  const { dataCount, isLoading, startTransition } = useTableContext();
-  const [{ pageSize, pageIndex }, setSearchParams] =
+  const { dataCount, isLoading, startTransition, dataSelected } =
+    useTableContext();
+  const [{ pageSize, pageIndex, selected }, setSearchParams] =
     useSearchParams(startTransition);
+  const [copiedText, copy] = useCopyToClipboard();
 
-  const totalPages = dataCount ? Math.ceil(dataCount / pageSize) : 0;
+  const totalPages = Math.ceil(dataCount / pageSize);
+
+  const userIdBatches = chunkArray(dataSelected?.data || [], 2);
+
+  const handleCopy = (text: string) => () => {
+    copy(text);
+    if (!text) {
+      toast.error("Nothing to copy");
+      return;
+    }
+
+    copy(text)
+      .then(() => {
+        toast.success("Copied", {
+          description: <div className="line-clamp-1">{copiedText || text}</div>,
+        });
+      })
+      .catch((error) => {
+        if (error instanceof Error) console.error(error.message);
+
+        toast.error("Failed to copy!");
+      });
+  };
+
+  const handleBan = async () => {
+    for (const batch of userIdBatches) {
+      const results = (await Promise.allSettled(
+        batch.map((user) => banUser(user)),
+      )) as {
+        status: string;
+        value: {
+          error?: string;
+          success?: string;
+        };
+      }[];
+
+      for (const result of results) {
+        // console.log(result.value);
+
+        if (result.value.error) toast.error(result.value.error);
+        if (result.value.success) toast.success(result.value.success);
+      }
+
+      // console.log("Batch done:", results);
+      await new Promise((r) => setTimeout(r, 200));
+    }
+  };
+
+  const handleUnban = async () => {
+    for (const batch of userIdBatches) {
+      const results = (await Promise.allSettled(
+        batch.map((user) => unbanUser(user)),
+      )) as {
+        status: string;
+        value: {
+          error?: string;
+          success?: string;
+        };
+      }[];
+
+      for (const result of results) {
+        // console.log(result.value);
+
+        if (result.value.error) toast.error(result.value.error);
+        if (result.value.success) toast.success(result.value.success);
+      }
+
+      // console.log("Batch done:", results);
+      await new Promise((r) => setTimeout(r, 200));
+    }
+  };
 
   return (
     <div className="flex items-center justify-between">
@@ -30,10 +112,39 @@ export function DataTablePagination() {
         <div className="text-muted-foreground flex-1 text-sm">
           {isLoading ? (
             <Skeleton className="h-5 w-40" />
+          ) : selected.length > 0 ? (
+            `${selected.length} of ${dataCount} row(s) selected`
           ) : (
             `${dataCount} row(s)`
           )}
         </div>
+        {selected.length > 0 && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <CustomButton
+                buttonLabel="Actions"
+                size={"sm"}
+                variant={"outline"}
+                className="h-8"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem
+                onClick={handleCopy(selected.map((id) => `${id}`).join("\n"))}
+              >
+                Copy id(s)
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem variant="destructive" onClick={handleBan}>
+                Ban
+              </DropdownMenuItem>
+              <DropdownMenuItem variant="default" onClick={handleUnban}>
+                Unban
+              </DropdownMenuItem>
+              <DropdownMenuItem variant="destructive">Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
