7052e25e32d875a26171a343f14f963dc8ddd908
Bobbaru-K <bobbaru311@gmail.com>
2025-10-24 20:25:42 +0300

    feat(ui): create format string related utils

---
+++ next-app/src/lib/utils/format-string.ts
@@ -0,0 +1,39 @@
+/**
+ *
+ * @param {string} string String to format.
+ *
+ * @example
+ * "Ștefănescu - Anca" // stefanescu-anca
+ * "Ștefănescu- Anca" // stefanescu-anca
+ * "Ștefănescu \t-    Anca" // stefanescu-anca
+ * "François Anca" // francois-anca
+ * "François  Anca" // francois-anca
+ * "François \t\tAnca" // francois-anca
+ * "---François Anca---" // francois-anca
+ * "François ---Anca" // francois-anca
+ */
+export const formatString = (string: string) => {
+  if (!string) return "";
+
+  const formatted = string
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replaceAll(/\s*-\s*/g, "-") // remove spaces around "-"
+    .replaceAll(/\s+/g, "-") // remove spaces
+    .replaceAll(/[^a-zA-Z0-9-]/g, "") // remove all special characters
+    .replace(/-{2,}/g, "-") // remove multiple dashes
+    .replace(/^-+|-+$/g, ""); // remove dashes from the beginning and the end
+
+  return formatted;
+};
+
+export const createFormattedSlug = (
+  firstName: string,
+  lastName: string,
+  username: string,
+) => {
+  const slug = firstName + "-" + username + "-" + lastName;
+
+  return formatString(slug);
+};
