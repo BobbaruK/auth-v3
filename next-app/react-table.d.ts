cae26a9eac11eee68dc986f8c32484d9f552c1a6
Bobbaru-K <bobbaru311@gmail.com>
2025-10-29 21:16:37 +0200

    fix(app): relocate and create declaration files

--- next-app/react-table.d.ts
+++ next-app/react-table.d.ts
@@ -1,10 +1,7 @@
 import "@tanstack/react-table"; //or vue, svelte, solid, qwik, etc.
 
 declare module "@tanstack/react-table" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     label: string;
   }
 }
-
-declare module "*.css";
