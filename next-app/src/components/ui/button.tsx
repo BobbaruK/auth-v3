57a3f66379d4c1002c4988d11511c21c7c095d98
Bobbaru-K <bobbaru311@gmail.com>
2025-11-03 17:57:21 +0200

    feat(ui): add shadcn's field and input group

--- next-app/src/components/ui/button.tsx
+++ next-app/src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils/index";
 import { Slot, Slottable } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
@@ -43,10 +43,12 @@ const buttonVariants = cva(
           "bg-[size:400%] bg-[linear-gradient(-45deg,var(--gradient-lime),var(--gradient-ocean),var(--gradient-wine),var(--gradient-rust))] animate-gradient-flow",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+        "icon-sm": "size-8",
+        "icon-lg": "size-10",
       },
     },
     defaultVariants: {
@@ -61,7 +63,7 @@ interface IconProps {
   iconPlacement: "left" | "right";
 }
 
-interface IconRefProps {
+export interface IconRefProps {
   icon?: never;
   iconPlacement?: undefined;
 }
